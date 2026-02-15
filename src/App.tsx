d9c0027ef0360675a0cf0fc9d1b8187e500d3f05
Luke Winters <lwinters136@gmail.com>
2026-02-15 18:52:41 -0500

    updated html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import mountains from "./assets/images/mountains.png";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>UD CISC275 with React Hooks and TypeScript</h1>
             </header>
-            <header>Luke Winters</header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-                <p>Hello World</p>
-            </p>
+            <p>This is a webpage</p>
+            <p>Luke Winters</p>
+            <img src={mountains} alt="A picture of mountains" />
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ul>
+            <p>Hello World</p>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            className="Box"
+                            style={{
+                                backgroundColor: "rgb(255,0,0)",
+                                width: "90%",
+                                height: "10vh",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div className="Box"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
