ecd6268324604b91ef30a84647be937906f2cdb7
Luke Winters <lwinters136@gmail.com>
2026-02-25 17:36:43 -0500

    wrote toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -62,7 +62,9 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const shortQuestion: string =
+        question.id.toString() + ": " + question.name.slice(0, 10);
+    return shortQuestion;
 }
 
 /**
