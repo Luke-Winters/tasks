8dd8c79db125a831db250a8ba4e739ee4d97dee0
Luke Winters <lwinters136@gmail.com>
2026-03-10 14:07:02 -0400

    wrote editMode

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -46,11 +46,3 @@ export function GiveAttempts(): React.JSX.Element {
         </div>
     );
 }
-
-// You will need a state to represent the number of attempts the user has left, and another state to represent the number of attempts they are requesting.
-// The initial number of attempts left should be 3.
-// The number of attempts left should be visible.
-// There should be a numeric input box where the user can specify their requested number of attempts.
-// There should be two buttons, one labeled use that decreases the attempts by one and one labeled gain that increases the attempts by the amount in the input box.
-// If the user attempts to request an invalid amount (e.g., the empty string "") that cannot be parsed as an integer, then do not change their number of attempts.
-// When the user is out of attempts, the use button should be disabled
